963815d77fbc1d65f4a8844fabbb85a9e293ac24
avbravo <avbravo@PC02-UTICA>
2017-03-28 14:53:55 -0500

    converter

--- src/main/java/com/avbravo/wizardjmoordb/generador/gen/ConverterGenerador.java
+++ src/main/java/com/avbravo/wizardjmoordb/generador/gen/ConverterGenerador.java
@@ -32,6 +32,7 @@ import javax.inject.Inject;
 @Named
 @RequestScoped
 public class ConverterGenerador implements Serializable {
+
     private static final long serialVersionUID = 1L;
     private static final Logger LOG = Logger.getLogger(ConverterGenerador.class.getName());
 
@@ -66,48 +67,6 @@ public class ConverterGenerador implements Serializable {
             if (Files.notExists(path, new LinkOption[]{LinkOption.NOFOLLOW_LINKS})) {
                 crearFile(ruta, entidad, archivo);
             }
-          
-
-            /**
-             * agregar los imports
-             */
-            if (!Utilidades.search(ruta, "import javax.enterprise.context.RequestScoped;")) {
-                Utilidades.add(ruta, "@Named", "import javax.enterprise.context.RequestScoped;", false);
-            }
-            if (!Utilidades.search(ruta, "import javax.inject.Inject;")) {
-                Utilidades.add(ruta, "@Named", "import javax.inject.Inject;", false);
-            }
-            if (!Utilidades.search(ruta, "import javax.faces.component.UIComponent;")) {
-                Utilidades.add(ruta, "@Named", "import javax.faces.component.UIComponent;", false);
-            }
-            if (!Utilidades.search(ruta, "import javax.faces.context.FacesContext;")) {
-                Utilidades.add(ruta, "@Named", "import javax.faces.context.FacesContext;", false);
-            }
-            if (!Utilidades.search(ruta, "import javax.faces.convert.Converter;")) {
-                Utilidades.add(ruta, "@Named", "import javax.faces.convert.Converter;", false);
-            }
-            if (!Utilidades.search(ruta, "import javax.inject.Named;")) {
-                Utilidades.add(ruta, "@Named", "import javax.inject.Named;", false);
-            }
-            
-            if (!Utilidades.search(ruta, "import " + proyectoEJB.getPaquete() + ".entity.*;")) {
-                Utilidades.add(ruta, "import javax.inject.Named;", "import " + proyectoEJB.getPaquete() + ".entity.*;", false);
-            }
-            if (!Utilidades.search(ruta, "import " + proyectoEJB.getPaquete() + ".ejb.*;")) {
-                Utilidades.add(ruta, "import javax.inject.Named;", "import " + proyectoEJB.getPaquete() + ".ejb.*;", false);
-            }
-            
-            /**
-             * generar los metodos
-             */
-            if (!Utilidades.search(ruta, "public Object getAsObject")) {
-                Utilidades.addBeforeEnd(ruta, "}", getAsObject(entidad, archivo));
-            }
-            if (!Utilidades.search(ruta, "public String getAsString(")) {
-                Utilidades.addBeforeEnd(ruta, "}", getAsString(entidad, archivo));
-            }
-
-            //maximo
         } catch (Exception e) {
             JSFUtil.addErrorMessage("procesar() " + e.getLocalizedMessage());
         }
@@ -115,78 +74,7 @@ public class ConverterGenerador implements Serializable {
 
     }
 
-    private String getAsObject(Entidad entidad, String archivo) {
-        try {
-            String atributoprimario = "";
-            Boolean esInteger = false;
-            String entity = Utilidades.letterToLower(archivo);
-            for (Atributos a : entidad.getAtributosList()) {
-                if (a.getEsPrimaryKey()) {
-                    atributoprimario = Utilidades.letterToUpper(a.getNombre());
-                    esInteger = a.getTipo().equals("Integer");
-                }
-            }
-            String texto = "";
-
-            texto += "    @Override" + "\r\n";
-            texto += "    public Object getAsObject(FacesContext facesContext, UIComponent uiComponent, String s) {" + "\r\n";
-            texto += "        " + archivo + " " + entity + " = new " + archivo + "();" + "\r\n";
-            texto += "         try{" + "\r\n";
-            texto += "            if(!s.equals(\"null\")){" + "\r\n";
-            if (esInteger) {
-                texto += "            " + entity + " = " + entity + "Facade.findById(Integer.parseInt(s));" + "\r\n";
-            } else {
-                texto += "            " + entity + " = " + entity + "Facade.findById(s);" + "\r\n";
-            }
-
-            texto += "              }" + "\r\n";
-            texto += "           } catch (Exception e) {" + "\r\n";
-            texto += "             JSFUtil.addErrorMessage(\"getAsObject()\" + e.getLocalizedMessage());" + "\r\n";
-            texto += "           }" + "\r\n";
-            texto += "           return " + entity+";" + "\r\n";
-            texto += "           }" + "\r\n";
-            return texto;
-        } catch (Exception e) {
-            JSFUtil.addErrorMessage("getAsObject()  " + e.getLocalizedMessage());
-        }
-        return "";
-    }
-
-    private String getAsString(Entidad entidad, String archivo) {
-        try {
-            String atributoprimario = "";
-            Boolean esInteger = false;
-            String entity = Utilidades.letterToLower(archivo);
-            for (Atributos a : entidad.getAtributosList()) {
-                if (a.getEsPrimaryKey()) {
-                    atributoprimario = Utilidades.letterToUpper(a.getNombre());
-                    esInteger = a.getTipo().equals("Integer");
-                }
-            }
-            String texto = "";
-
-            texto += "    @Override" + "\r\n";
-            texto += "    public String getAsString(FacesContext facesContext, UIComponent uiComponent, Object o) {" + "\r\n";
-            texto += "        String r = \"\";" + "\r\n";
-            texto += "        try {" + "\r\n";
-            texto += "            if (o instanceof " + archivo + ") {" + "\r\n";
-            texto += "                " + archivo + " " + entity + " = (" + archivo + ") o;" + "\r\n";
-            texto += "                r = String.valueOf(" + entity + ".get" + atributoprimario + "());" + "\r\n";
-
-            texto += "            }else if (o instanceof String) {" + "\r\n";
-            texto += "               r = (String) o;" + "\r\n";
-            texto += "            }" + "\r\n";
-            texto += "        } catch (Exception e) {" + "\r\n";
-            texto += "            JSFUtil.addErrorMessage(\"getAsString()\" + e.getLocalizedMessage());" + "\r\n";
-            texto += "        }" + "\r\n";
-            texto += "        return r;" + "\r\n";
-            texto += "        }" + "\r\n";
-            return texto;
-        } catch (Exception e) {
-            JSFUtil.addErrorMessage("getAsString()  " + e.getLocalizedMessage());
-        }
-        return "";
-    }
+   
 
     /**
      * deleteAll
@@ -202,7 +90,7 @@ public class ConverterGenerador implements Serializable {
             String entity = Utilidades.letterToLower(archivo);
             for (Atributos a : entidad.getAtributosList()) {
                 if (a.getEsPrimaryKey()) {
-                  atributoprimario = Utilidades.letterToUpper(a.getNombre());
+                    atributoprimario = Utilidades.letterToUpper(a.getNombre());
                     esInteger = a.getTipo().equals("Integer");
                 }
             }
@@ -226,10 +114,11 @@ public class ConverterGenerador implements Serializable {
                     fw.write(" * and open the template in the editor." + "\r\n");
                     fw.write("*/" + "\r\n");
                     fw.write("package " + proyectoEJB.getPaquete() + ".converter;" + "\r\n");
-                        fw.write("" + "\r\n");
+                    fw.write("" + "\r\n");
                     fw.write("import " + proyectoEJB.getPaquete() + ".entity.*;" + "\r\n");
                     fw.write("import " + proyectoEJB.getPaquete() + ".ejb.*;" + "\r\n");
-                    fw.write("import " + proyectoEJB.getPaquete() + ".generales.JSFUtil;" + "\r\n");
+                    fw.write("import com.avbravo.avbravoutils.JsfUtil;" + "\r\n");
+                    fw.write("import java.util.Optional;" + "\r\n");
                     fw.write("import javax.enterprise.context.RequestScoped;" + "\r\n");
                     fw.write("import javax.inject.Inject;" + "\r\n");
                     fw.write("import javax.faces.component.UIComponent;" + "\r\n");
@@ -240,7 +129,7 @@ public class ConverterGenerador implements Serializable {
                     fw.write("" + "\r\n");
                     fw.write("/**" + "\r\n");
                     fw.write(" *" + "\r\n");
-                    fw.write(" * @author " +mySesion.getUsername()+ "\r\n");
+                    fw.write(" * @author " + mySesion.getUsername() + "\r\n");
                     fw.write(" */" + "\r\n");
 
                     fw.write("@Named" + "\r\n");
@@ -252,22 +141,27 @@ public class ConverterGenerador implements Serializable {
 
                     fw.write("" + "\r\n");
                     fw.write("    @Override" + "\r\n");
-                    fw.write("    public Object getAsObject(FacesContext facesContext, UIComponent uiComponent, String s) {" + "\r\n");
+                    fw.write("    public Object getAsObject(FacesContext facesContext, UIComponent uiComponent, "                            + "String s) {" + "\r\n");
                     fw.write("        " + archivo + " " + entity + " = new " + archivo + "();" + "\r\n");
-                    fw.write("         try{" + "\r\n");
+                    fw.write("        try{" + "\r\n");
                     fw.write("            if(!s.equals(\"null\")){" + "\r\n");
-                    if (esInteger) {
-                        fw.write("            " + entity + " = " + entity + "Facade.findById(Integer.parseInt(s));" + "\r\n");
-                    } else {
-                        fw.write("            " + entity + " = " + entity + "Facade.findById(s);" + "\r\n");
+                    fw.write("               " + archivo + " b = new " + archivo + "();" + "\r\n");
+                    if( esInteger){
+                        fw.write("               b.set" + Utilidades.letterToUpper(atributoprimario) + "(Integer.parseInt(s));" + "\r\n");
+                    }else{
+                        fw.write("               b.set" + Utilidades.letterToUpper(atributoprimario) + "(s);" + "\r\n");
                     }
-
-                    fw.write("              }" + "\r\n");
-                    fw.write("           } catch (Exception e) {" + "\r\n");
-                    fw.write("             JSFUtil.addErrorMessage(\"getAsObject()\" + e.getLocalizedMessage());" + "\r\n");
-                    fw.write("           }" + "\r\n");
-                    fw.write("           return " + entity+";" + "\r\n");
-                    fw.write("           }" + "\r\n");
+                    
+                    fw.write("               Optional<" + archivo + "> optional = " + entity + "Facade.findById(b);" + "\r\n");
+                    fw.write("               if (optional.isPresent()) {" + "\r\n");
+                    fw.write("               " + entity + "= optional.get();  " + "\r\n");
+                    fw.write("               }   " + "\r\n");
+                    fw.write("             }" + "\r\n");
+                    fw.write("          } catch (Exception e) {" + "\r\n");
+                    fw.write("             JsfUtil.addErrorMessage(\"getAsObject()\" + e.getLocalizedMessage());" + "\r\n");
+                    fw.write("          }" + "\r\n");
+                    fw.write("          return " + entity + ";" + "\r\n");
+                    fw.write("      }" + "\r\n");
                     fw.write("" + "\r\n");
 
                     fw.write("" + "\r\n");
@@ -283,7 +177,7 @@ public class ConverterGenerador implements Serializable {
                     fw.write("               r = (String) o;" + "\r\n");
                     fw.write("            }" + "\r\n");
                     fw.write("        } catch (Exception e) {" + "\r\n");
-                    fw.write("            JSFUtil.addErrorMessage(\"getAsString()\" + e.getLocalizedMessage());" + "\r\n");
+                    fw.write("            JsfUtil.addErrorMessage(\"getAsString()\" + e.getLocalizedMessage());" + "\r\n");
                     fw.write("        }" + "\r\n");
                     fw.write("        return r;" + "\r\n");
                     fw.write("        }" + "\r\n");
